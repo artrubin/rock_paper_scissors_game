3e5a8e98bfd6489617ed7201ad0ce334d39e731b
Artem Rubin <151726742+artrubin@users.noreply.github.com>
2024-03-18 15:38:20 +0100

    Update rock_paper_scissors_random.py
    Logic updated to be able to paly best of N number of games

--- rock_paper_scissors_random.py
+++ rock_paper_scissors_random.py
@@ -1,15 +1,18 @@
 from random import choice, randint
-play = 1
-while play == 1:
-    # randomly assigns values to compute_choice
-    computer_choice = choice(["rock", "scissors", "paper"])
+user_wins = 0
+computer_wins = 0
+score = 3
 
+while user_wins < score and computer_wins < score:
+    computer_choice = choice(["rock", "scissors", "paper"])
+    print(f"User Score: {user_wins} Computer Score: {computer_wins}")
     print("Rock...")
     print("Paper...")
     print("Scissors...")
 
-    user_choice = input("Please enter your choice >>").lower()
-
+    user_choice = input("Please enter your choice >> ").lower()
+    if user_choice == "quit" or user_choice == "q":
+        break
     if user_choice not in ["rock", "scissors", "paper"]:
         print("Wrong Entry. You can only enter 'rock', 'paper', or 'scissors'")
     else:
@@ -17,19 +20,29 @@ while play == 1:
             print(f"Computer plays '{computer_choice}', it is a tie!!! ")
         elif user_choice == "rock":
             if computer_choice == "scissors":
-                print(f"Computer plays '{computer_choice}', you won!!!")
+                print(f"Computer plays '{computer_choice}', you won this round!!!")
+                user_wins += 1
             elif computer_choice == "paper":
-                print(f"Computer plays '{computer_choice}', you lost!!!")
+                print(f"Computer plays '{computer_choice}', you lost this round!!!")
+                computer_wins += 1
         elif user_choice == "paper":
             if computer_choice == "scissors":
-                print(f"Computer plays '{computer_choice}', you lost!!!")
+                print(f"Computer plays '{computer_choice}', you lost round!!!")
+                computer_wins += 1
             elif computer_choice == "rock":
-                print(f"Computer plays '{computer_choice}, you won!!!")
+                print(f"Computer plays '{computer_choice}, you won this round!!!")
+                user_wins += 1
         elif user_choice == "scissors":
             if computer_choice == "rock":
-                print(f"Computer plays '{computer_choice}', you lost!!!")
+                print(f"Computer plays '{computer_choice}', you lost this round!!!")
+                computer_wins += 1
             elif computer_choice == "paper":
-                print(f"Computer plays '{computer_choice}', you won!!!")
-
-    play = int(input("Enter '1' if you would like to play the game, enter 'O' if you would like to stop >>"))
+                print(f"Computer plays '{computer_choice}', you won this round!!!")
+                user_wins += 1
+if user_wins > computer_wins:
+    print("CONGRATULATION, YOU WON THE GAME!!!")
+elif user_wins == computer_wins:
+    print ("IT'S A TIE...)
+else:
+    print("OH NO :( THE COMPUTER WON THE GAME ...")
 
